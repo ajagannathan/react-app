71286cde2fc5542e416af1b14cc0557334548a38
ajagannathan <anand.jagan@gmail.com>
2021-02-24 22:10:34 +0530

    Tested Event handler and this operator binding

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,7 +5,32 @@ class Counter extends Component {
     //that contains any data the component needs
     state = {
         count: 1,
+        tagList: ["tag1", "tag2", "tag3"],
+        //tagList: [],
     };
+    rendertags() {
+        if (this.state.tagList.length === 0) return <p>No tags</p>;
+        return this.state.tagList.map((tag) => <li key={tag}>{tag}</li>);
+    }
+    constructor() {
+        super();
+        //Using the bind function this always references the current object
+        this.handleIncrementOnClickEvent = this.handleIncrementOnClickEvent.bind(
+            this
+        );
+    }
+    handleIncrementOnClickEvent() {
+        console.log("Increment clicked");
+        //Since this function id a standalone function object and not an Counter object method
+        //this operator is undefined and thus this line throws error.
+        //One way to solve it is by binding the function to this object in the class constructor
+        console.log(this.state.count);
+    }
+    //Another way to solve the this binding issue is by using arrow functions
+    handleIncrementOnClickEventArrow = () => {
+        console.log(`Arrow click ${this.state.count}`);
+    };
+
     render() {
         // 1. This is a jsx expression that Babel compiles to a call to React.createElement()
         //    Although we dont explicitly call React.createElement we nee to import React
@@ -13,19 +38,33 @@ class Counter extends Component {
         // 2. When creating multiple components (for eg. h1, button) Babel doesnt know how to
         //    compile to a react element. One way is to wrap it in a <div>
         return (
-            //This div gets written into <div id='root'></div> of public/index.html
-            //The div here does not do anything and is unnecessary. In that case we can use React.Fragment
+            //1. This div gets written into <div id='root'></div> of public/index.html
+            //2. The div here does not do anything and is unnecessary. In that case we can use React.Fragment
             //     <div>
             //     <h1>Hello React</h1>
             //     <button>Increment</button>
             //   </div>
-            //We can also embed expressions that can change dynamically as shown below within <span></span>
+            //3. We can also embed expressions that can change dynamically as shown below within <span></span>
+            //4. When adding a list, key={} attribute is given for React to identify each item in the list
+            //   So that when the state of the element changes in virtual DOM it knows where to update in actual DOM
             <React.Fragment>
                 <h1>Hello React</h1>
                 <span className={this.getClassType()}>
                     {this.validateCount()}
                 </span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button
+                    onClick={this.handleIncrementOnClickEventArrow} //We pass reference to the function and dont call
+                    className="btn btn-secondary btn-sm"
+                >
+                    Increment
+                </button>
+                <ul>
+                    {
+                        /* {this.state.tagList.map((tag) => (
+                        <li key={tag}>{tag}</li>
+                    ))} */ this.rendertags()
+                    }
+                </ul>
             </React.Fragment>
         );
     }
